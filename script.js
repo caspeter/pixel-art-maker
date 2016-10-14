acfea19245e23bd419d717c672ec77454e40de61
Cassie Peterson <cas.j.peters@gmail.com>
2016-10-14 17:00:10 -0600

    grid works, click create black to work, working on color picker

--- script.js
+++ script.js
@@ -4,19 +4,46 @@ var grid = document.querySelector('#grid');
 // var row = document.createElement('div');
 // var pixel = document.createElement('div');
 
+
+ // --- THE GRID ---
+
 var createRow = function(numberOfRows) {
     for (var i = 0; i < numberOfRows; i++) {
       // console.log("test");
         var row = document.createElement('section');
-        var numbOfBlocks = 70;
-        for (var j = 0; j < numbOfBlocks; j++) {
+        var numbOfPixels = 70;
+        for (var j = 0; j < numbOfPixels; j++) {
             var pixel = document.createElement('article');
             pixel.classList.add('pixelStyle');
             row.appendChild(pixel);
+            pixel.addEventListener('click', fillPixel)
         }
         grid.appendChild(row);
     }
-    console.log(row);
+    // console.log(row);
 }
 
-createRow(20);
+createRow(25);
+
+ // --- CLICK TO CHANGE BLOCK TO BLACK
+
+var grabPixel = document.querySelectorAll('article');
+// console.log(grabPixel);
+
+function fillPixel (event) {
+  event.target.style.backgroundColor = 'black';
+};
+
+ // --- COLOR PICKER
+
+var colors = document.querySelector('.color')
+// console.log(colors);
+
+// var getText =
+
+var getColor = function (event) {
+  event.target.id;
+  console.log(event.target.id);
+}
+
+colors.addEventListener('click', getColor)
